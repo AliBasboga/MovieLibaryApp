0e0a9674c82a77eda295b2bbab1d625bb72b7f0e
Ali Başboğa <alibasboga885@gmail.com>
2025-07-15 11:43:30 +0300

    Movie-movieItem finsh

--- app/src/main/java/com/example/movielibaryapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/movielibaryapp/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val Pink41 = Color(0xFFFAF7F3)
+val Pink42 = Color(0xFFF0E4D3)
+val Pink43 = Color(0xFFDCC5B2)
+val Pink44 = Color(0xFFD9A299)
+
